e1333801d8e7b1362022dde8639a6e2ce93bfccc
ThibaultLatrille <thibault.latrille@ens-lyon.org>
2018-09-03 12:42:28 +0200

    Script refactor, removed all syntaxing sugar depending on zsh

--- detection-variants-pipeline.sh
+++ detection-variants-pipeline.sh
@@ -107,14 +107,15 @@ samtools sort -@ 4 HG02024_SRR822251.bam > HG02024_SRR822251.sorted.bam
 # Command: gatk AddOrReplaceReadGroups
 # Input: alignment (.bam) and read group (Read group identifier, DNA preparation library identifier, Platform, Platform Unit, Sample)
 # Ouput: annotated alignment (.bam)
-gatk AddOrReplaceReadGroups -I HG02024_SRR822251.sorted.bam -O HG02024_SRR822251.sorted.RG.bam \
-    --RGID SRR822251 --RGLB Pond-206419 --RGPL illumina --RGPU C1E0PACXX121221.6.tagged_373 --RGSM HG02024 --RGPI 160
+gatk AddOrReplaceReadGroups -I HG02024_SRR822251.sorted.bam -O daughter.bam \
+                            --RGID SRR822251 --RGLB Pond-206419 --RGPL illumina \
+                            --RGPU C1E0PACXX121221.6.tagged_373 --RGSM HG02024 --RGPI 160
 
 # Visualize read group
 # Command: samtools view -H && grep
 # Input: annotated alignment (.bam)
 # Ouput: read group
-samtools view -H HG02024_SRR822251.sorted.RG.bam | grep '@RG'
+samtools view -H daughter.bam | grep '@RG'
 
 # Compute statistics of the alignment
 # Command: samtools-stats
@@ -140,34 +141,34 @@ samtools index daughter.bam
 ###########################
 
 # Variables definition
-ftpSeqFolder=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3 # Ftp folder from 1000Genomes project
-RGID=SRR361100 # Read group identifier
-RGSM=HG02025 # Sample
-RGPL=illumina # Platform/technology used to produce the read
-RGLB=Catch-88584 # DNA preparation library identifier
-RGPU=BI.PE.110902_SL-HBC_0182_AFCD046MACXX.2.tagged_851 # Platform Unit
-RGPI=96 # Insert size
+FTP_SEQ_FOLDER=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3 # Ftp folder from 1000Genomes project
+RUN_ID=SRR361100 # Read group identifier
+SAMPLE_NAME=HG02025 # Sample
+INSTRUMENT_PLATFORM=illumina # Platform/technology used to produce the read
+LIBRARY_NAME=Catch-88584 # DNA preparation library identifier
+RUN_NAME=BI.PE.110902_SL-HBC_0182_AFCD046MACXX.2.tagged_851 # Platform Unit
+INSERT_SIZE=96 # Insert size
 
 # Download paired sequencing reads for the mother
 # Command: wget
 # Input: url (http:// or ftp://)
 # Ouput: compressed sequencing reads (.fastq.gz)
-wget ${ftpSeqFolder}/data/${RGSM}/sequence_read/${RGID}_1.filt.fastq.gz
-wget ${ftpSeqFolder}/data/${RGSM}/sequence_read/${RGID}_2.filt.fastq.gz
+wget ${FTP_SEQ_FOLDER}/data/${SAMPLE_NAME}/sequence_read/${RUN_ID}_1.filt.fastq.gz
+wget ${FTP_SEQ_FOLDER}/data/${SAMPLE_NAME}/sequence_read/${RUN_ID}_2.filt.fastq.gz
 
 # Map, filter, and sort the paired sequencing reads of the mother against the reference genome
 # Command: bwa mem && samtools view && samtools sort
 # Input: indexed reference (.fa), and compressed sequencing reads (.fastq.gz)
 # Ouput: sorted alignment (.bam)
-bwa mem -t 4 -M Homo_sapiens.Chr20.fa ${RGID}_1.filt.fastq.gz ${RGID}_2.filt.fastq.gz | samtools view -@ 4 -bh -f 3 | samtools sort -@ 4 > ${RGSM}.${RGID}.sorted.bam
+bwa mem -t 4 -M Homo_sapiens.Chr20.fa ${RUN_ID}_1.filt.fastq.gz ${RUN_ID}_2.filt.fastq.gz | samtools view -@ 4 -bh -f 3 | samtools sort -@ 4 > ${SAMPLE_NAME}.${RUN_ID}.sorted.bam
 
-# Add Read group 
+# Add Read group
 # Command: gatk AddOrReplaceReadGroups
 # Input: alignment (.bam) and read group
 # Ouput: alignment (.bam)
-gatk AddOrReplaceReadGroups -I ${RGSM}.${RGID}.sorted.bam -O mother.bam \
-                            --RGID ${RGID} --RGLB ${RGLB} --RGPL ${RGPL} \
-                            --RGPU ${RGPU} --RGSM ${RGSM} --RGPI ${RGPI}
+gatk AddOrReplaceReadGroups -I ${SAMPLE_NAME}.${RUN_ID}.sorted.bam -O mother.bam \
+                            --RGID ${RUN_ID} --RGLB ${LIBRARY_NAME} --RGPL ${INSTRUMENT_PLATFORM} \
+                            --RGPU ${RUN_NAME} --RGSM ${SAMPLE_NAME} --RGPI ${INSERT_SIZE}
 
 # Index the alignment
 # Command: samtools index
@@ -180,74 +181,73 @@ samtools index mother.bam
 ###########################
 
 # Variables definition
-RGSM=HG02026 # Sample
+SAMPLE_NAME=HG02026 # Sample
 
 # Download index file containing sequencing runs information
 # Command: wget
 # Input: url (http:// or ftp://)
 # Ouput: text file (.index)
-wget ${ftpSeqFolder}/20130502.phase3.analysis.sequence.index -O 20130502.phase3.index
+wget ${FTP_SEQ_FOLDER}/20130502.phase3.analysis.sequence.index -O 20130502.phase3.index
 
 # Filter paired exome sequencing runs related to father (HG02026)
 # Command: grep && grep -v
-# Input: text file (.index)
-# Ouput: filtered text file (.index)
-grep ${RGSM} 20130502.phase3.index | grep "exome" | grep 'PAIRED' | grep -v 'Solexa' | grep -v 'from blood' | grep -v '_1.filt.fastq.gz' | grep -v '_2.filt.fastq.gz' > father.index
+# Input: tab-separated values file (.index)
+# Ouput: filtered comma-separated values file (.index)
+grep ${SAMPLE_NAME} 20130502.phase3.index | grep "exome" | grep 'PAIRED' | grep -v 'Solexa' | grep -v 'from blood' | grep -v '_1.filt.fastq.gz' | grep -v '_2.filt.fastq.gz' | sed 's/\t/,/g' > father.index
 
-# Variables definition
-bamlist="" # The list of .bam files created for each sequencing run
+# File containing the list of alignments (each line is a .bam file)
+# This file is necessary to merge multiple alignments into a single alignment.
+# Command: touch
+# Input: file name
+# Ouput: empty file (.bamlist)
+touch ${SAMPLE_NAME}.bamlist
 
 # for each sequencing run (the first 10), align to the reference, sort, add read group and index
-head -10 father.index | while read LINE; do
-    
+head -6 father.index | while IFS="," read FASTQ_FILE MD5 RUN_ID STUDY_ID STUDY_NAME CENTER_NAME SUBMISSION_ID SUBMISSION_DATE SAMPLE_ID SAMPLE_NAME POPULATION EXPERIMENT_ID INSTRUMENT_PLATFORM INSTRUMENT_MODEL LIBRARY_NAME RUN_NAME RUN_BLOCK_NAME INSERT_SIZE LIBRARY_LAYOUT PAIRED_FASTQ WITHDRAWN WITHDRAWN_DATE COMMENT READ_COUNT BASE_COUNT ANALYSIS_GROUP
+do
+
     # Variables definition
-    fastqFtp=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 1)
-    RGID=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 3) # Read group identifier
-    RGPL=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 12) # Platform/technology used to produce the read
-    RGLB=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 14) # DNA preparation library identifier
-    RGPU=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 15) # Platform Unit
-    RGPI=$(echo ${LINE} | tr -s '\t' ';' | cut -d ';' -f 16) # Insert size
-    fastqFtp1=${fastqFtp/.filt.fastq.gz/_1.filt.fastq.gz} # URL of the fasta file
-    fastqFtp2=${fastqFtp/.filt.fastq.gz/_2.filt.fastq.gz} # URL of the fasta file (pairing file)
+    FASTQ_FILE_1=${FASTQ_FILE/.filt.fastq.gz/_1.filt.fastq.gz} # Path of the fasta file in the FTP folder
+    FASTQ_FILE_2=${FASTQ_FILE/.filt.fastq.gz/_2.filt.fastq.gz} # Path of the fasta file in the FTP folder (pairing file)
 
     # Download paired sequencing reads for the father
     # Command: wget
     # Input: url (http:// or ftp://)
     # Ouput: compressed sequencing reads (.fastq.gz)
-    wget ${ftpSeqFolder}/${fastqFtp1} -O ${RGSM}.${RGID}_1.fastq.gz
-    wget ${ftpSeqFolder}/${fastqFtp2} -O ${RGSM}.${RGID}_2.fastq.gz
-    
+    wget ${FTP_SEQ_FOLDER}/${FASTQ_FILE_1} -O ${SAMPLE_NAME}.${RUN_ID}_1.fastq.gz
+    wget ${FTP_SEQ_FOLDER}/${FASTQ_FILE_2} -O ${SAMPLE_NAME}.${RUN_ID}_2.fastq.gz
+
     # Map, filter, and sort the paired reads of the sequencing run against the reference genome
     # Command: bwa mem && samtools view && samtools sort
     # Input: indexed reference (.fa), and compressed sequencing reads (.fastq.gz)
     # Ouput: sorted alignment (.bam)
-    bwa mem -t 4 -M Homo_sapiens.Chr20.fa ${RGSM}.${RGID}_1.fastq.gz ${RGSM}.${RGID}_2.fastq.gz | samtools view -@ 4 -bh -f 3 | samtools sort -@ 4 > ${RGSM}.${RGID}.sorted.bam
-    
-    # Add Read group 
+    bwa mem -t 4 -M Homo_sapiens.Chr20.fa ${SAMPLE_NAME}.${RUN_ID}_1.fastq.gz ${SAMPLE_NAME}.${RUN_ID}_2.fastq.gz | samtools view -@ 4 -bh -f 3 | samtools sort -@ 4 > ${SAMPLE_NAME}.${RUN_ID}.sorted.bam
+
+    # Add Read group
     # Command: gatk AddOrReplaceReadGroups
     # Input: alignment (.bam) and read group
     # Ouput: alignment (.bam)
-    gatk AddOrReplaceReadGroups -I ${RGSM}.${RGID}.sorted.bam -O ${RGSM}.${RGID}.sorted.RG.bam \
-                                --RGID ${RGID} --RGLB ${RGLB} --RGPL ${RGPL} \
-                                --RGPU ${RGPU} --RGSM ${RGSM} --RGPI ${RGPI}
+    gatk AddOrReplaceReadGroups -I ${SAMPLE_NAME}.${RUN_ID}.sorted.bam -O ${SAMPLE_NAME}.${RUN_ID}.sorted.RG.bam \
+                                --RGID ${RUN_ID} --RGLB ${LIBRARY_NAME} --RGPL ${INSTRUMENT_PLATFORM} \
+                                --RGPU ${RUN_NAME} --RGSM ${SAMPLE_NAME} --RGPI ${INSERT_SIZE}
 
-    samtools view -H ${RGSM}.${RGID}.sorted.RG.bam | grep '@RG'
-    
+    samtools view -H ${SAMPLE_NAME}.${RUN_ID}.sorted.RG.bam | grep '@RG'
+
     # Index the alignment
     # Command: samtools index
     # Input: alignment (.bam)
     # Ouput: indexed alignment (.bam.bai)
-    samtools index ${RGSM}.${RGID}.sorted.RG.bam
-    
-    # Concatenate the alignment to the list of alignments
-    bamlist="${RGSM}.${RGID}.sorted.RG.bam ${bamlist}"
+    samtools index ${SAMPLE_NAME}.${RUN_ID}.sorted.RG.bam
+
+    # Append the file name (.bam) to the list of alignments that will be merged
+    echo ${SAMPLE_NAME}.${RUN_ID}.sorted.RG.bam >> ${SAMPLE_NAME}.bamlist
 done
 
 # Merge the list of alignments into a single file
 # Command: samtools merge
-# Input: alignments (.bam) separated by space ' '
+# Input: file containing the list of alignments (each line is a .bam file)
 # Ouput: alignment (.bam)
-samtools merge father.bam $(echo ${bamlist})
+samtools merge -b ${SAMPLE_NAME}.bamlist father.bam
 
 # Index the alignment
 # Command: samtools index
@@ -257,102 +257,102 @@ samtools index father.bam
 
 samtools view -H father.bam | grep '@RG'
 
-
-#####################
-## Variant Calling ##
-#####################
-
-# GATK best practices pipeline:
-# 1. Mark duplicates
-# 2. Realigned around known indels
-# 3. Recalibrate errors using known SNP
-
-# The Ftp folder for the known indels and snps
-ftpMapping=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/other_mapping_resources/
-# Use of the Mills and 1000G gold standard dataset for known indels
-known_indels=Mills_and_1000G_gold_standard.indels.b38.primary_assembly.vcf.gz
-# Use of the dbSNP v142 dataset
-known_snps=ALL_20141222.dbSNP142_human_GRCh38.snps.vcf.gz
-
-# Downloading the known indels and SNPs if necessary
-wget ${ftpMapping}/${known_indels} -O ./${known_indels}
-wget ${ftpMapping}/${known_indels}.tbi -O ./${known_indels}.tbi
-wget ${ftpMapping}/${known_snps} -O ./${known_snps}
-wget ${ftpMapping}/${known_snps}.tbi -O ./${known_snps}.tbi
-
-gatk MarkDuplicates \
-    -I father.bam \
-    -O father.markDup.bam \
-    --METRICS_FILE father.markDup_metrics.txt
-
-samtools index father.markDupRG.bam
-
-gatk CreateSequenceDictionary -R Homo_sapiens.Chr20.fa
-
-# 2. Realigned around known indels
-gatk  RealignerTargetCreator \
-    -R Homo_sapiens.Chr20.fa \
-    -I father.markDupRG.bam \
-    -o father.intervals \
-    --known ./${known_indels}
-
-gatk IndelRealigner \
-    -R Homo_sapiens.Chr20.fa \
-    -I father.markDupRG.bam \
-    -o father.markDupRG.realigned.bam \
-    --targetIntervals father.intervals \
-    -known ${known_indels}
-
-# 3. Recalibrate errors using known SNP
-gatk BaseRecalibrator \
-    -R Homo_sapiens.Chr20.fa \
-    -I father.markDupRG.realigned.bam \
-    -o father.markDupRG.realigned.table \
-    -cov ReadGroupCovariate \
-    -cov QualityScoreCovariate \
-    -cov CycleCovariate -cov ContextCovariate \
-    -knownSites ${known_snps}
-
-gatk PrintReads \
-    -R Homo_sapiens.Chr20.fa \
-    -I father.markDupRG.realigned.bam \
-    -o father.markDupRG.realigned.recal.bam \
-    -BQSR father.markDupRG.realigned.table \
-    --disable_bam_indexing
-
-
-gatk HaplotypeCaller \
-    -R Homo_sapiens.Chr20.fa \
-    -I father.markDupRG.realigned.recal.bam \
-    -o father.gvcf \
-    --genotyping_mode DISCOVERY \
-    -variant_index_type LINEAR \
-    -variant_index_parameter 128000 \
-    --emitRefConfidence GVCF
-
-# Converting gVCF to VCF
-gatk GenotypeGVCFs \
-    -R Homo_sapiens.Chr20.fa \
-    --variant father.gvcf \
-    --variant mother.gvcf \
-    --variant daughter.gvcf \
-    -o trio.vcf
-
-
-gatk PhaseByTransmission \
-        -R Homo_sapiens.Chr20.fa \
-        -V trio.vcf \
-        -ped ./20130606_g1k.ped.txt \
-        -o trio.phased.vcf
-
-gatk VariantEval \
-        -R Homo_sapiens.Chr20.fa \
-        -o trio.phased.VE.txt \
-        --eval:set1 trio.vcf \
-        --eval:set2 trio.phased.vcf
-
-gatk GenotypeConcordance \
-        -R Homo_sapiens.Chr20.fa \
-        -eval trio.phased.vcf \
-        -comp trio.vcf \
-        -o trio.GC.txt
+#
+######################
+### Variant Calling ##
+######################
+#
+## GATK best practices pipeline:
+## 1. Mark duplicates
+## 2. Realigned around known indels
+## 3. Recalibrate errors using known SNP
+#
+## The Ftp folder for the known indels and snps
+#ftpMapping=ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/GRCh38_reference_genome/other_mapping_resources/
+## Use of the Mills and 1000G gold standard dataset for known indels
+#known_indels=Mills_and_1000G_gold_standard.indels.b38.primary_assembly.vcf.gz
+## Use of the dbSNP v142 dataset
+#known_snps=ALL_20141222.dbSNP142_human_GRCh38.snps.vcf.gz
+#
+## Downloading the known indels and SNPs if necessary
+#wget ${ftpMapping}/${known_indels} -O ./${known_indels}
+#wget ${ftpMapping}/${known_indels}.tbi -O ./${known_indels}.tbi
+#wget ${ftpMapping}/${known_snps} -O ./${known_snps}
+#wget ${ftpMapping}/${known_snps}.tbi -O ./${known_snps}.tbi
+#
+#gatk MarkDuplicates \
+#    -I father.bam \
+#    -O father.markDup.bam \
+#    --METRICS_FILE father.markDup_metrics.txt
+#
+#samtools index father.markDupRG.bam
+#
+#gatk CreateSequenceDictionary -R Homo_sapiens.Chr20.fa
+#
+## 2. Realigned around known indels
+#gatk  RealignerTargetCreator \
+#    -R Homo_sapiens.Chr20.fa \
+#    -I father.markDupRG.bam \
+#    -o father.intervals \
+#    --known ./${known_indels}
+#
+#gatk IndelRealigner \
+#    -R Homo_sapiens.Chr20.fa \
+#    -I father.markDupRG.bam \
+#    -o father.markDupRG.realigned.bam \
+#    --targetIntervals father.intervals \
+#    -known ${known_indels}
+#
+## 3. Recalibrate errors using known SNP
+#gatk BaseRecalibrator \
+#    -R Homo_sapiens.Chr20.fa \
+#    -I father.markDupRG.realigned.bam \
+#    -o father.markDupRG.realigned.table \
+#    -cov ReadGroupCovariate \
+#    -cov QualityScoreCovariate \
+#    -cov CycleCovariate -cov ContextCovariate \
+#    -knownSites ${known_snps}
+#
+#gatk PrintReads \
+#    -R Homo_sapiens.Chr20.fa \
+#    -I father.markDupRG.realigned.bam \
+#    -o father.markDupRG.realigned.recal.bam \
+#    -BQSR father.markDupRG.realigned.table \
+#    --disable_bam_indexing
+#
+#
+#gatk HaplotypeCaller \
+#    -R Homo_sapiens.Chr20.fa \
+#    -I father.markDupRG.realigned.recal.bam \
+#    -o father.gvcf \
+#    --genotyping_mode DISCOVERY \
+#    -variant_index_type LINEAR \
+#    -variant_index_parameter 128000 \
+#    --emitRefConfidence GVCF
+#
+## Converting gVCF to VCF
+#gatk GenotypeGVCFs \
+#    -R Homo_sapiens.Chr20.fa \
+#    --variant father.gvcf \
+#    --variant mother.gvcf \
+#    --variant daughter.gvcf \
+#    -o trio.vcf
+#
+#
+#gatk PhaseByTransmission \
+#        -R Homo_sapiens.Chr20.fa \
+#        -V trio.vcf \
+#        -ped ./20130606_g1k.ped.txt \
+#        -o trio.phased.vcf
+#
+#gatk VariantEval \
+#        -R Homo_sapiens.Chr20.fa \
+#        -o trio.phased.VE.txt \
+#        --eval:set1 trio.vcf \
+#        --eval:set2 trio.phased.vcf
+#
+#gatk GenotypeConcordance \
+#        -R Homo_sapiens.Chr20.fa \
+#        -eval trio.phased.vcf \
+#        -comp trio.vcf \
+#        -o trio.GC.txt
